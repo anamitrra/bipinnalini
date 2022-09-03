a43462d31657e678f2a1951fcb1c3b6fb594a932
Anirban <85803381+anirbandsharma@users.noreply.github.com>
2022-09-03 22:09:34 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = "localhost";
+$username = "root1";
+$password = "pass";
+$database = "bipinnalini";
+
+$con=mysqli_connect($server,$username,$password,$database)or die("can't connect...");
+?>
